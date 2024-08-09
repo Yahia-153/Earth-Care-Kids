d326675f744c19685a7696893bd849fe8ef6b86e
Yahia-153 <yahiametwally153@gmail.com>
2024-08-10 00:35:09 +0300

    audio

--- js sheets/main.js
+++ js sheets/main.js
@@ -1,4 +1,4 @@
-const audio = new Audio('hi kids i am Zain i .mp3');
+const audio = new Audio('audio/hi kids i am Zain i .mp3');
 function hiSound() {
         if (audio.paused) {
             audio.play();
@@ -6,7 +6,7 @@ function hiSound() {
             audio.pause();
         }
     }
-    const audio2 = new Audio('HEY-KIDS-JUMP-INTO-O.mp3');
+    const audio2 = new Audio('audio/HEY-KIDS-JUMP-INTO-O.mp3');
     function gameSound() {
         if (audio2.paused) {
             audio2.play();
